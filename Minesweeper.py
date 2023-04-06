c5fadb6ab1ef0cc1234c2081d97510e4e4535432
이진영 <jakerhee321@gmail.com>
2023-04-06 13:14:22 +0900

    If safe

--- Minesweeper.py
+++ Minesweeper.py
@@ -97,9 +97,17 @@ class Board:
         str_len = int(len(string_rep) / self.dim_size)
         string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
 
-        return 
+        return string_rep
     
     
 def play(dim_size=10, num_bombs=10):
     board = Board(dim_size, num_bombs)
-    pass
+    safe = True
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*',input("Where Dig? Input as row, col: "))
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try another spot.")
+            continue
+        safe = board.dig(row, col)
